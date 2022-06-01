7198f362f51417cf25c74e04a9764c800168de6c
vanmanhaes <100885533+vanmanhaes@users.noreply.github.com>
2022-06-01 20:42:11 -0300

    Add files via upload

---
+++ aula230522.r
@@ -0,0 +1,62 @@
+#escrever o meu comentário
+#carregar base de dados
+
+5+4 #adição
+
+6-2 #subtração
+
+15*9 #multiplicação
+
+45/9 #divião
+
+10^4 #potência
+
+sqrt(625)#raiz quadrada
+
+#criando vetores
+A <- c(1, 2, 3, 4, 5)
+B = c(1:5)
+
+A
+B
+
+A+B
+
+C = c("a","b","c","d")
+
+#vamos fazer uma tabela
+#notas de 3 avaliações do Raphael
+
+notas.raphael = c(8, 8.6, 8.8)
+
+#notas de 3 avaliações do Pedro
+
+notas.pedro = c(7.3, 6.7, 7)
+
+#consultar as notas
+notas.raphael
+
+notas.pedro
+
+#criando uma tabela
+notas = data.frame(notas.raphael, notas.pedro)
+notas
+
+tipo = c("Av1", "Av2", "Trab")
+
+notas = data.frame(tipo, notas.pedro, notas.raphael)
+notas
+
+
+
+
+install.packages(rmarkdown)
+
+
+
+
+
+
+
+
+
